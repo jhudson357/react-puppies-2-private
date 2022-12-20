37813ffa1a36441b0ef68342fad3d321780b5255
James Hudson <jmh357@comcast.net>
2022-12-20 17:09:15 -0600

    connect front and back end

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
-import { Route, Routes, Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { Route, Routes, Link, useNavigate } from 'react-router-dom'
 import './App.css'
 import AddPuppy from './pages/AddPuppy/AddPuppy'
+import * as puppyService from '../src/services/puppyService'
 
 function App() {
   const [puppies, setPuppies] = useState([])
+  
+  const navigate = useNavigate()
 
-  const handleAddPuppy = newPuppyData => {
-    setPuppies([...puppies, newPuppyData])
+  const handleAddPuppy = async newPuppyData => {
+    const newPuppy = await puppyService.create(newPuppyData)
+    setPuppies([...puppies, newPuppy])
+    navigate('/')
   }
+
+  useEffect(() => {
+    const fetchAllPuppies = async () => {
+      const puppyData = await puppyService.getAll()
+      setPuppies(puppyData)
+    }
+    fetchAllPuppies()
+  }, [])
 
   return (
     <div className="App">
---
+++ src/services/puppyService.js
@@ -0,0 +1,22 @@
+const BASE_URL = `${process.env.REACT_APP_BACK_END_SERVER_URL}/api/puppies`
+
+async function create(puppy) {
+  const res = await fetch(BASE_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(puppy)
+  })
+  return res.json()
+}
+
+async function getAll() {
+  const res = await fetch(BASE_URL)
+  return res.json()
+}
+
+export {
+  create, 
+  getAll,
+}
